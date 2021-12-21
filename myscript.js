88bc7bf3a0b179341c351bbc7422e601ab15e3bd
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-21 02:34:51 +0100

    color

--- myscript.js
+++ myscript.js
@@ -1,8 +1,14 @@
 let scatola = document.getElementById("ms_container");
 
+// creazione scatole
 for(let i = 1; i <= 100; i++){
     {
         console.log('iterazione: ', i);
         scatola.innerHTML += '<div class=box>' + i + '</div>';
     }
+}
+
+// function per cambiare colore al quadrato dopo il click
+function changeColorBox(){
+    
 }
